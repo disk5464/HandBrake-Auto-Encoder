28dfa6c58b46d636c2a5f9b6da7441d6d65cb83c
disk5464 <36386147+disk5464@users.noreply.github.com>
2026-03-31 10:57:45 -0400

    Add files via upload

---
+++ AutoEncoder_OnDemand_V1.3.ps1
@@ -0,0 +1,102 @@
+#This script will take in a directory, figure out which files are larger than 1GB, use handbreak to reencode them, then delete the orginal file.
+#Last modified: 3/31/26
+#Created by: Disk5464
+#Version 1.0: Inital Commit.
+#Version 1.1: Added some logging and ability to provide a text file
+#Version 1.2: Added an exceptions CSV to avoid processing files that will not be able to be compressed
+#Version 1.3: Hardcoded the input directory so that it can be run as a scheduled task. Added the ability to add unprocessable files to the exceptions list automatically. Added a variable for the script directory to make it more portable.
+#######################################################################################
+#Get the working directory. This allows the script to run from any location
+$ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+#Start a transcript to track any logs
+$TranscriptName = get-date -Format mm-dd-yyyy
+Start-Transcript -Path "$ScriptDirectory\Logs\Log_$TranscriptName.txt"
+
+#Change directories to where handbreak is installed
+Set-Location "C:\Users\ADMIN\Desktop\Important Stuff\Tools\Handbreak Encoder"
+
+#######################################################################################
+#Get either the single folder location of a list of dirctories to encode.
+$Single12 = $null
+while ($Single12 -ne "1" -and $Single12 -ne "2")
+{
+    $Single12 = Read-Host "Do you want to encode a single folder or multiple folders? (1/2)"
+}
+
+#If the user just wants 1 directory get it, if they want multiple get it via a text file.
+if ($Single12 -eq 1) {
+    $DirectoriesToEncode = Read-Host "Please enter the FDQN of the directory you want to encode"
+}
+elseif ($Single12 -eq 2) 
+{
+    #Create the file browser, spawn it, then get the content of the text file
+    Write-Host "Please select the text file that contains the list of directories to encode." -ForegroundColor Green
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+        InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+        Filter = 'Text (*.txt)|*.txt'
+    }
+    $FileBrowser.ShowDialog()
+    $DirectoriesToEncode = Get-Content -Path $FileBrowser.FileName
+}
+
+#Import the list of expections
+$ExpectionsListCSV = "$ScriptDirectory\Exceptions List.csv"
+$expectionsList = Import-Csv -Path $ExpectionsListCSV
+
+#######################################################################################
+foreach ($Directory in $DirectoriesToEncode)
+{
+    #Get all of the files that need to be compressed
+    $FilesToCompress = Get-ChildItem -Path $Directory -Recurse -File -filter "*.mkv" | Where-Object { $_.Length -gt 1GB } 
+
+    #Foreach file that needs to be encoded 
+    foreach ($file in $FilesToCompress)
+    {
+        #Check to make sure th file is not on the exceptions list
+        if($file.fullname -notin $expectionsList."File Name")
+        {
+            #Set a variable for the output name
+            Write-Host "Encoding" $file.Fullname
+            $parts = $file.Name -split '\.'
+            $OutFileNameBase = if ($parts.Length -gt 1) { $parts[0..($parts.Length-2)] -join '.' } else { $file.Name }
+
+            #Set the output FDQN with the new file name
+            $FDQNOutName = $file.DirectoryName  + "\" + $OutFileNameBase + ".mp4"
+            
+            #Try to encode the file.
+            try {./HandBrakeCLI.exe --input $file.FullName --output $FDQNOutName --preset="Fast 1080p30" --subtitle 1,2,3 --keep-subname --all-audio }
+            catch {Write-Host "Error Encoding $($file.Fullname)" -ForegroundColor Red}
+
+            #Try and delete the orginal file
+            try { 
+                if( $file.length -gt (get-childitem -path $FDQNOutName).length)
+                {
+                    write-host "Deleting the source file $($file.FullName)" -ForegroundColor Green
+                    Remove-item -Path $file.FullName
+                }
+                else {
+                    write-host "New file is larger than the orginal, deleting the new file $($FDQNOutName)" -ForegroundColor Yellow
+                    Remove-item -Path $FDQNOutName
+
+                    #Create a new object and add it to the exception list
+                    $Outobject = [pscustomobject]@{
+                        "File Name" = $file.FullName
+                        "Root Directory" = $file.DirectoryName
+                        "File Size (MB)" = [Math]::Round( ((get-childitem -path $file.FullName).length / 1024 /1024), 2)
+                        "File Size (GB)" = [Math]::Round( ((get-childitem -path $file.FullName).length / 1024 /1024 /1024), 2)
+                    }
+                    
+                    #Add the file info to the exceptions list CSV
+                    $Outobject | Export-Csv -Path $ExpectionsListCSV -Append -NoTypeInformation
+                }
+            }
+            catch { Write-Host "Error deleteing the source or new file $($file.FullName)" -ForegroundColor Red }
+        }
+        else {Write-Host "$($File.name) is on the exceptions list and will not be processed."}
+    }
+}
+
+#######################################################################################
+Stop-Transcript
